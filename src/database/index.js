d054436ef2f117e696cb2b64cfc35fba80e43398
Janderson Martins <jandersonmartins10@gmail.com>
2018-03-14 20:34:03 -0300

    Removing crawler from this repository

--- src/database/index.js
+++ src/database/index.js
@@ -13,6 +13,8 @@ module.exports = {
   connect
 }
 
+// TODO: Create pool connection function
+
 /**
  * Connect with mongodb.
  *
--- src/dataset/config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = {
-  baseUrl: 'http://dados.gov.br',
-  features: [
-    'farmacia_popular_brasil'
-  ]
-}
