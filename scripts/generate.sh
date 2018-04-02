2a6e9c3b89df753e53b13753083975b907ad90da
Janderson Martins <jandersonmartins10@gmail.com>
2018-04-02 15:39:41 -0300

    chore: .travis.yml and scripts/generator.sh
    Fix certs out path and define scripts that generates certs in travis file.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,4 +2,4 @@
 
 openssl req -x509 -nodes -newkey rsa:2048 -keyout cert.key -out cert.crt -days 365 \
   -subj "/C=BR/ST=Denial/L=Springfield/O=Dis/CN=www.saudify.com" \
-  -keyout csr/saudify.key  -out csr/saudify.cert
+  -keyout certs/saudify.key  -out certs/saudify.cert
