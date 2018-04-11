e5e133717b5e04e97a9fb2daecc9b40e0a69985f
Janderson <jandersonmartins10@gmail.com>
2018-04-11 20:19:42 -0300

    refactor: Connect database
    Connecting database in server startup

--- src/config/bootstrap.js
+++ src/config/bootstrap.js
@@ -1,14 +1,16 @@
 'use strict'
 
+const database = require('../database')
+
 /**
  * Starts application.
  * @param {Server} server
  */
 function bootstrap (server) {
-  // TODO: Connect database after listen
-  server.listen(process.env.SERVER_PORT, () =>
+  server.listen(process.env.SERVER_PORT, async () => {
+    await database.connect(process.env.MONGO_URI)
     console.log('saudify-api running')
-  )
+  })
 }
 
 module.exports = bootstrap
--- src/config/bootstrap.test.js
+++ src/config/bootstrap.test.js
@@ -1,13 +1,28 @@
 'use strict'
 
 const bootstrap = require('./bootstrap')
+const database = require('../database')
 
 describe('Unit: config/bootstrap', function () {
   describe('default exports', function () {
+    let databaseSpy
+
+    before(function () {
+      databaseSpy = sinon.spy(database, 'connect')
+    })
+
+    after(function () {
+      databaseSpy.restore()
+    })
+
     it('should starts server', function () {
-      const mock = { listen: sinon.spy() }
+      const mock = { listen: (port, cb) => cb() }
+      const listenSpy = sinon.spy(mock, 'listen')
+
       bootstrap(mock)
-      expect(mock.listen).to.have.been.calledWith(process.env.SERVER_PORT)
+
+      expect(listenSpy).to.have.been.calledWith(process.env.SERVER_PORT)
+      expect(databaseSpy).to.be.calledWith(process.env.MONGO_URI)
     })
   })
 })
