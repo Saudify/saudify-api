98931e08f8cd1d49c1631ecf22106224a6522dee
Janderson Martins <jandersonmartins10@gmail.com>
2018-04-19 14:13:32 -0300

    test(middleware/not-found): Remove unnecessary describe

--- src/lib/http/middlewares/not-found.test.js
+++ src/lib/http/middlewares/not-found.test.js
@@ -4,24 +4,22 @@ const httpMocks = require('node-mocks-http')
 const notFound = require('./not-found')
 
 describe('Unit: lib/http/middlewares/not-found', function () {
-  describe('respond 404 json', function () {
-    it('should response with 404 json', function () {
-      const request = {}
-      const response = httpMocks.createResponse()
+  it('should response with 404 json', function () {
+    const request = {}
+    const response = httpMocks.createResponse()
 
-      notFound(request, response)
+    notFound(request, response)
 
-      const jsonResponse = JSON.parse(response._getData())
+    const jsonResponse = JSON.parse(response._getData())
 
-      expect(jsonResponse).to.eql({
-        code: 404,
-        message: 'Route not found',
-        status: 'error'
-      })
-
-      expect(response.statusCode).to.equal(404)
-      expect(response._isEndCalled()).to.be.true
-      expect(response._isJSON()).to.be.true
+    expect(jsonResponse).to.eql({
+      code: 404,
+      message: 'Route not found',
+      status: 'error'
     })
+
+    expect(response.statusCode).to.equal(404)
+    expect(response._isEndCalled()).to.be.true
+    expect(response._isJSON()).to.be.true
   })
 })
