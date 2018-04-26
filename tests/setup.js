7d3ceba32008af33da622c5ad57161c868c0a0bb
Janderson <jandersonmartins10@gmail.com>
2018-04-26 12:14:36 -0300

    test(route/feature-collections): Improve assertion
    Assert in each field returned by the endpoint

--- tests/setup.js
+++ tests/setup.js
@@ -9,11 +9,13 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
 const chai = require('chai')
+const sinonChai = require('sinon-chai')
+const dateChai = require('chai-date-string')
 const request = require('./helpers/request')
 
 chai.use(sinonChai)
+chai.use(dateChai)
 
 global.request = request()
 global.expect = chai.expect
